7bd8296dcbf026d1da97a1109615fc6f138339d7
chochozabe <cintamani_whyk@naver.com>
2021-11-01 23:37:34 +0900

    Refactor : 주석/서비스 헬스 체크 컨트롤러 추가

--- src/main/java/com/whykk/houseutils/controller/BrokerageQueryController.java
+++ src/main/java/com/whykk/houseutils/controller/BrokerageQueryController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author chochozabe
+ *
+ * 중계수수료가 얼마인지 조회하는 컨트롤러
+ */
 @RestController
 public class BrokerageQueryController {
 
--- src/main/java/com/whykk/houseutils/policy/BrokeragePolicy.java
+++ src/main/java/com/whykk/houseutils/policy/BrokeragePolicy.java
@@ -1,5 +1,8 @@
 package com.whykk.houseutils.policy;
 
+/**
+ * @author chochozabe
+ */
 public interface BrokeragePolicy {
     BrokerageRule createBrokerageRule(Long price);
 
